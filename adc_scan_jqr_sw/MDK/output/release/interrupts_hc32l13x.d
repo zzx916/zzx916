b7b5eafcfeed22aef9ad24ac1f1901d0aff4f7e9
夏中州 <13125153669@163.com>
2022-02-19 15:39:38 +0800

    first commit

---
+++ adc_scan_jqr_sw/MDK/output/release/interrupts_hc32l13x.d
@@ -0,0 +1,17 @@
+./output/release/interrupts_hc32l13x.o: ..\common\interrupts_hc32l13x.c \
+  ..\driver\inc\ddl.h ..\common\base_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  ..\common\board_stkhc32l13x.h ..\common\hc32l13x.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\common\system_hc32l13x.h ..\driver\inc\sysctrl.h \
+  ..\driver\inc\ddl.h ..\common\interrupts_hc32l13x.h \
+  ..\source\ddl_device.h ..\common\interrupts_hc32l13x.h
---
+++ adc_scan_jqr_sw/MDK/output/release/uart.d
@@ -0,0 +1,17 @@
+./output/release/uart.o: ..\driver\src\uart.c ..\driver\inc\uart.h \
+  ..\driver\inc\ddl.h ..\common\base_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\assert.h \
+  ..\common\board_stkhc32l13x.h ..\common\hc32l13x.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\dell\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\common\system_hc32l13x.h ..\driver\inc\sysctrl.h \
+  ..\common\interrupts_hc32l13x.h ..\driver\inc\ddl.h \
+  ..\source\ddl_device.h
